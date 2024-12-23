bc78c8aad56b50d3b13253d63ac326352a48c791
Pascal Ernst <pascalernst@Mac.fritz.box>
2024-12-23 19:33:43 +0100

    Initial commit

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,23 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  "slack"
+
+  # Utility Tools
+  "bitwarden"
+
+  # Entertainment Tools
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "chromium"
+  "firefox"
+]
