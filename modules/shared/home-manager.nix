d8a8269824e1745cb2892f9a8c4442e4b59d9531
LinuCC <linucc@linu.cc>
2025-03-03 14:21:04 +0100

    feat(git): Add `br` alias for recent branches

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -62,6 +62,9 @@ in
     lfs = {
       enable = true;
     };
+    aliases = {
+      br = "for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(align:35)%(color:yellow)%(refname:short)%(color:reset)%(end) - %(color:red)%(objectname:short)%(color:reset) - %(align:40)%(contents:subject)%(end) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'";
+    };
     extraConfig = {
       init.defaultBranch = "main";
       core = {
